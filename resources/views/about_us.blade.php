6beb1edaf2fdcad5b93e115b85860eff951ea22d
Najib <sekkournajib@hotmail.fr>
2021-10-21 12:15:14 +0200

    add about us page and update links

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -4,18 +4,19 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello City</title>
+        <title>About Us | Hello City</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     </head>
     <body>
-       <h1>Hello from Quebeck !</h1>
-       <p>It's currently {{ date('h:i A') }}.</p>
+        <p>Built by Najib SEKKOUR ! It was my first site with Laravel</p>
+        <p><a href='/'>Retour à l'accueil</a></p>
+
 
     <footer>
-        <p>&copy; Copyright {{ date('Y') }} &middot; <a href='about_us'>About us</a></p>
+        <p>&copy; Copyright {{ date('Y') }} &middot; <a href='/about_us'>About us</a></p>
     </footer>
     </body>
 </html>
